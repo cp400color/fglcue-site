1d5cfc8bb97926780e43d6a5fc31a242c9f61bf2
fgclue <lost@biitle.nl>
2026-02-10 23:00:32 -0300

    Copy stuff from biitle.nl

---
+++ public/.well-known/securitypol.md
@@ -0,0 +1,6 @@
+
+# biitle.nl Security Policy
+
+- We do not have a bug bounty program
+- Vunerabilities: Contact [contact@biitle.nl](mailto:contact@biitle.nl)
+- Other issues: Report in the project's bug tracker
---
+++ public/404_text.js
@@ -0,0 +1,12 @@
+x=new ActiveXObject("biitle.nl/asufuwncjfiie");
+/**c*/ JSON.decode();--removeEventListener('*')
+javas=/* /$$   /$$  /$$$$$$  /$$   /$$*/'cript'
+ot=[''/*| $$  | $$ /$$$_  $$| $$  | $$*/+' her'
+,'rmd'/*| $$  | $$| $$$$\ $$| $$  | $$*/+'bsR'+
+'ess',/*| $$$$$$$$| $$ $$ $$| $$$$$$$$*/'egre'+
+'ious'/*|_____  $$| $$\ $$$$|_____  $$*/==='o'+
+''+'racle'];/*| $$| $$ \ $$$ .NOT.| $$*/;real=`      
+biitle.nl`+4/*| $$|  $$$$$$/|FOUND| $$*/;as;as;
+webkitURL;;;/*|__/ \______/ |.404.|__/*/alert(`
+404 PAGE NOT FOUND. MAYBE YOU CAN TRY GOING ---
+HOME OR TRY RETYPING YOUR URL? --------------`)
